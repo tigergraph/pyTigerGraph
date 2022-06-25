97daf5306a9b84d083a7768d6099fa495115f3fe
Bill Shi <bill.shi@tigergraph.com>
2022-06-25 16:01:26 -0700

    feat: install udf from url

--- pyTigerGraph/pyTigerGraphGSQL.py
+++ pyTigerGraph/pyTigerGraphGSQL.py
@@ -8,8 +8,8 @@ import sys
 from typing import Union
 from urllib.parse import urlparse
 
+import requests
 from pyTigerDriver import GSQL_Client
-from typing import TYPE_CHECKING, Union
 
 from pyTigerGraph.pyTigerGraphBase import pyTigerGraphBase
 from pyTigerGraph.pyTigerGraphException import TigerGraphException
@@ -48,34 +48,46 @@ class pyTigerGraphGSQL(pyTigerGraphBase):
                 # TODO Expected type 'Tuple[str, int]', got 'Tuple[bytes, int]' instead
 
             try:
-                certcontent = open(self.certLocation, 'w')
+                certcontent = open(self.certLocation, "w")
                 certcontent.write(Res)
                 certcontent.close()
             except Exception:  # TODO Too broad exception clause
                 self.certLocation = "/tmp/my-cert.txt"
 
-                certcontent = open(self.certLocation, 'w')
+                certcontent = open(self.certLocation, "w")
                 certcontent.write(Res)
                 certcontent.close()
             if os.stat(self.certLocation).st_size == 0:
                 raise TigerGraphException(
-                    "Certificate download failed. Please check that the server is online.", None)
+                    "Certificate download failed. Please check that the server is online.",
+                    None,
+                )
 
         try:
 
             if self.downloadCert:
                 if not self.certPath:
                     self.certPath = self.certLocation
-                self.Client = GSQL_Client(urlparse(self.host).netloc, version=self.version,
+                self.Client = GSQL_Client(
+                    urlparse(self.host).netloc,
+                    version=self.version,
                     username=self.username,
                     password=self.password,
-                    cacert=self.certPath, gsPort=self.gsPort,
-                    restpp=self.restppPort, debug=self.debug)
+                    cacert=self.certPath,
+                    gsPort=self.gsPort,
+                    restpp=self.restppPort,
+                    debug=self.debug,
+                )
             else:
-                self.Client = GSQL_Client(urlparse(self.host).netloc, version=self.version,
+                self.Client = GSQL_Client(
+                    urlparse(self.host).netloc,
+                    version=self.version,
                     username=self.username,
                     password=self.password,
-                    gsPort=self.gsPort, restpp=self.restppPort, debug=self.debug)
+                    gsPort=self.gsPort,
+                    restpp=self.restppPort,
+                    debug=self.debug,
+                )
             self.Client.login()
             self.gsqlInitiated = True
             return True
@@ -121,7 +133,7 @@ class pyTigerGraphGSQL(pyTigerGraphBase):
             print("Couldn't initialize the client. See above error.")
             sys.exit(1)
 
-    def installUDF(self, ExprFunctions: str = '', ExprUtil: str = '') -> None:
+    def installUDF(self, ExprFunctions: str = "", ExprUtil: str = "") -> None:
         """Install User Defined Function (UDF) to the database. See https://docs.tigergraph.com/gsql-ref/current/querying/func/query-user-defined-functions for details on UDFs.
 
         Args:
@@ -134,25 +146,41 @@ class pyTigerGraphGSQL(pyTigerGraphBase):
             Status of the installation.
         """
         if ExprFunctions:
-            with open(ExprFunctions) as infile:     
-                res = self._req("PUT", 
-                    url="{}/gsqlserver/gsql/userdefinedfunction?filename=ExprFunctions".format(self.gsUrl),
-                    authMode='pwd',
-                    data=infile.read(),
-                    resKey=None)
-                if (res["error"] == False):
-                    print("ExprFunctions installed succesfully !") 
-                else:
-                    print("Failed to install ExprFunctions")
-        
+            if ExprFunctions.startswith("http"):
+                # A url. Fetch from online.
+                data = requests.get(ExprFunctions).text
+            else:
+                # A local file. Read from disk.
+                with open(ExprFunctions) as infile:
+                    data = infile.read()
+            res = self._req(
+                "PUT",
+                url="{}/gsqlserver/gsql/userdefinedfunction?filename=ExprFunctions".format(self.gsUrl),
+                authMode="pwd",
+                data=data,
+                resKey=None
+            )
+            if res["error"] == False:
+                print("ExprFunctions installed succesfully")
+            else:
+                print("Failed to install ExprFunctions")
+
         if ExprUtil:
-            with open(ExprUtil) as infile:
-                res = self._req("PUT", 
-                    url="{}/gsqlserver/gsql/userdefinedfunction?filename=ExprUtil".format(self.gsUrl),
-                    authMode='pwd',
-                    data=infile.read(),
-                    resKey=None)
-                if (res["error"] == False):
-                    print("ExprFunctions installed succesfully !") 
-                else:
-                    print("Failed to install ExprFunctions")
+            if ExprUtil.startswith("http"):
+                # A url. Fetch from online.
+                data = requests.get(ExprUtil).text
+            else:
+                # A local file. Read from disk.
+                with open(ExprUtil) as infile:
+                    data = infile.read()
+            res = self._req(
+                "PUT",
+                url="{}/gsqlserver/gsql/userdefinedfunction?filename=ExprUtil".format(self.gsUrl),
+                authMode="pwd",
+                data=data,
+                resKey=None
+            )
+            if res["error"] == False:
+                print("ExprUtil installed succesfully")
+            else:
+                print("Failed to install ExprUtil")
