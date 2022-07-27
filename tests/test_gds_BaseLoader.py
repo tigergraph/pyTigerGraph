5244cfe62921836879389d1b8b9f463433eb54a3
Bill Shi <bill.shi@tigergraph.com>
2022-07-27 16:57:42 -0700

    feat: treat undirected edges specially in parsing

--- tests/test_gds_BaseLoader.py
+++ tests/test_gds_BaseLoader.py
@@ -42,7 +42,8 @@ class TestGDSBaseLoader(unittest.TestCase):
                     "ToVertexTypeName": "Paper",
                     "time": "INT",
                     "is_train": "BOOL",
-                    "is_val": "BOOL"
+                    "is_val": "BOOL",
+                    "IsDirected": True,
                 }
             },
         )
@@ -58,7 +59,7 @@ class TestGDSBaseLoader(unittest.TestCase):
                     "y": "INT",
                     "train_mask": "BOOL",
                     "val_mask": "BOOL",
-                    "test_mask": "BOOL"
+                    "test_mask": "BOOL",
                 }
             },
         )
@@ -67,7 +68,8 @@ class TestGDSBaseLoader(unittest.TestCase):
             {
                 "Cite3": {
                     "FromVertexTypeName": "Paper3",
-                    "ToVertexTypeName": "Paper3"
+                    "ToVertexTypeName": "Paper3",
+                    "IsDirected": True,
                 }
             },
         )
@@ -85,8 +87,8 @@ class TestGDSBaseLoader(unittest.TestCase):
             self.loader._validate_vertex_attributes(["x ", " y"]), ["x", "y"]
         )
         self.assertDictEqual(
-            self.loader._validate_vertex_attributes({"Paper": ["x ", " y"]}, True), 
-            {"Paper": ["x", "y"]}
+            self.loader._validate_vertex_attributes({"Paper": ["x ", " y"]}, True),
+            {"Paper": ["x", "y"]},
         )
         # Wrong input
         with self.assertRaises(ValueError):
@@ -98,9 +100,9 @@ class TestGDSBaseLoader(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.loader._validate_vertex_attributes(1)
         with self.assertRaises(ValueError):
-            self.loader._validate_vertex_attributes(["x"], is_hetero = True)
+            self.loader._validate_vertex_attributes(["x"], is_hetero=True)
         with self.assertRaises(ValueError):
-            self.loader._validate_vertex_attributes({"Paper": ["x"]}, is_hetero = False)    
+            self.loader._validate_vertex_attributes({"Paper": ["x"]}, is_hetero=False)
 
     def test_validate_edge_attributes(self):
         # Empty input
@@ -112,11 +114,14 @@ class TestGDSBaseLoader(unittest.TestCase):
         self.assertDictEqual(self.loader._validate_edge_attributes({}, True), {})
         # Extra spaces
         self.assertListEqual(
-            self.loader._validate_edge_attributes(["time ", "is_train"]), ["time", "is_train"]
+            self.loader._validate_edge_attributes(["time ", "is_train"]),
+            ["time", "is_train"],
         )
         self.assertDictEqual(
-            self.loader._validate_edge_attributes({"Cite": ["time ", "is_train"]}, True), 
-            {"Cite": ["time", "is_train"]}
+            self.loader._validate_edge_attributes(
+                {"Cite": ["time ", "is_train"]}, True
+            ),
+            {"Cite": ["time", "is_train"]},
         )
         # Wrong input
         with self.assertRaises(ValueError):
@@ -128,9 +133,9 @@ class TestGDSBaseLoader(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.loader._validate_edge_attributes(1)
         with self.assertRaises(ValueError):
-            self.loader._validate_edge_attributes(["time"], is_hetero = True)
+            self.loader._validate_edge_attributes(["time"], is_hetero=True)
         with self.assertRaises(ValueError):
-            self.loader._validate_edge_attributes({"Cite": ["time"]}, is_hetero = False)  
+            self.loader._validate_edge_attributes({"Cite": ["time"]}, is_hetero=False)
 
     def test_read_vertex(self):
         read_task_q = Queue()
@@ -154,7 +159,7 @@ class TestGDSBaseLoader(unittest.TestCase):
         truth = pd.read_csv(
             io.StringIO(raw),
             header=None,
-            names=["vid", "x", "y", "train_mask", "is_seed"]
+            names=["vid", "x", "y", "train_mask", "is_seed"],
         )
         assert_frame_equal(data, truth)
         data = data_q.get()
@@ -168,13 +173,26 @@ class TestGDSBaseLoader(unittest.TestCase):
         read_task_q.put(raw)
         read_task_q.put(None)
         self.loader._read_data(
-            exit_event, read_task_q, data_q, "edge", "dataframe", [], [], [], {},
-            ["x", "time"], ["y"], ["is_train"], 
-            {"x": "FLOAT", "time": "INT", "y": "INT", "is_train": "BOOL"} 
+            exit_event,
+            read_task_q,
+            data_q,
+            "edge",
+            "dataframe",
+            [],
+            [],
+            [],
+            {},
+            ["x", "time"],
+            ["y"],
+            ["is_train"],
+            {"x": "FLOAT", "time": "INT", "y": "INT", "is_train": "BOOL"},
         )
         data = data_q.get()
-        truth = pd.read_csv(io.StringIO(raw), header=None, 
-            names=["source", "target", "x", "time", "y", "is_train"])
+        truth = pd.read_csv(
+            io.StringIO(raw),
+            header=None,
+            names=["source", "target", "x", "time", "y", "is_train"],
+        )
         assert_frame_equal(data, truth)
         data = data_q.get()
         self.assertIsNone(data)
@@ -185,7 +203,7 @@ class TestGDSBaseLoader(unittest.TestCase):
         exit_event = Event()
         raw = (
             "99,1 0 0 1 ,1,0,1\n8,1 0 0 1 ,1,1,1\n",
-            "1,2,0.1,2021,1,0\n2,1,1.5,2020,0,1\n"
+            "1,2,0.1,2021,1,0\n2,1,1.5,2020,0,1\n",
         )
         read_task_q.put(raw)
         read_task_q.put(None)
@@ -199,21 +217,23 @@ class TestGDSBaseLoader(unittest.TestCase):
             ["y"],
             ["train_mask", "is_seed"],
             {"x": "INT", "y": "INT", "train_mask": "BOOL", "is_seed": "BOOL"},
-            ["x", "time"], ["y"], ["is_train"], 
-            {"x": "FLOAT", "time": "INT", "y": "INT", "is_train": "BOOL"} 
+            ["x", "time"],
+            ["y"],
+            ["is_train"],
+            {"x": "FLOAT", "time": "INT", "y": "INT", "is_train": "BOOL"},
         )
         data = data_q.get()
         vertices = pd.read_csv(
             io.StringIO(raw[0]),
             header=None,
             names=["vid", "x", "y", "train_mask", "is_seed"],
-            dtype="object"
+            dtype="object",
         )
         edges = pd.read_csv(
-            io.StringIO(raw[1]), 
-            header=None, 
+            io.StringIO(raw[1]),
+            header=None,
             names=["source", "target", "x", "time", "y", "is_train"],
-            dtype="object"
+            dtype="object",
         )
         assert_frame_equal(data[0], vertices)
         assert_frame_equal(data[1], edges)
@@ -226,7 +246,7 @@ class TestGDSBaseLoader(unittest.TestCase):
         exit_event = Event()
         raw = (
             "99,1 0 0 1 ,1,0,Alex,1\n8,1 0 0 1 ,1,1,Bill,0\n",
-            "99,8,0.1,2021,1,0\n8,99,1.5,2020,0,1\n"
+            "99,8,0.1,2021,1,0\n8,99,1.5,2020,0,1\n",
         )
         read_task_q.put(raw)
         read_task_q.put(None)
@@ -246,14 +266,18 @@ class TestGDSBaseLoader(unittest.TestCase):
                 "name": "STRING",
                 "is_seed": "BOOL",
             },
-            ["x", "time"], ["y"], ["is_train"], 
-            {"x": "DOUBLE", "time": "INT", "y": "INT", "is_train": "BOOL"} 
+            ["x", "time"],
+            ["y"],
+            ["is_train"],
+            {"x": "DOUBLE", "time": "INT", "y": "INT", "is_train": "BOOL"},
         )
         data = data_q.get()
         self.assertIsInstance(data, pygData)
         assert_close_torch(data["edge_index"], torch.tensor([[0, 1], [1, 0]]))
-        assert_close_torch(data["edge_feat"], 
-            torch.tensor([[0.1, 2021], [1.5, 2020]], dtype=torch.double))
+        assert_close_torch(
+            data["edge_feat"],
+            torch.tensor([[0.1, 2021], [1.5, 2020]], dtype=torch.double),
+        )
         assert_close_torch(data["edge_label"], torch.tensor([1, 0]))
         assert_close_torch(data["is_train"], torch.tensor([False, True]))
         assert_close_torch(data["x"], torch.tensor([[1, 0, 0, 1], [1, 0, 0, 1]]))
@@ -290,7 +314,7 @@ class TestGDSBaseLoader(unittest.TestCase):
             [],
             [],
             [],
-            {}
+            {},
         )
         data = data_q.get()
         self.assertIsInstance(data, pygData)
@@ -304,8 +328,8 @@ class TestGDSBaseLoader(unittest.TestCase):
         data_q = Queue(4)
         exit_event = Event()
         raw = (
-            "People,99,1 0 0 1 ,1,0,Alex,1\nPeople,8,1 0 0 1 ,1,1,Bill,0\nCompany,1,0.3,0\n",
-            "Work,99,8,0.1,2021,1,0\nWork,8,99,1.5,2020,0,1\n"
+            "People,99,1 0 0 1 ,1,0,Alex,1\nPeople,8,1 0 0 1 ,1,1,Bill,0\nCompany,2,0.3,0\n",
+            "Colleague,99,8,0.1,2021,1,0\nColleague,8,99,1.5,2020,0,1\nWork,99,2\nWork,2,99\n",
         )
         read_task_q.put(raw)
         read_task_q.put(None)
@@ -318,37 +342,65 @@ class TestGDSBaseLoader(unittest.TestCase):
             {"People": ["x"], "Company": ["x"]},
             {"People": ["y"]},
             {"People": ["train_mask", "name", "is_seed"], "Company": ["is_seed"]},
-            {"People": {
-                "x": "LIST:INT",
-                "y": "INT",
-                "train_mask": "BOOL",
-                "name": "STRING",
-                "is_seed": "BOOL"},
-             "Company": {"x": "FLOAT", "is_seed": "BOOL"}
+            {
+                "People": {
+                    "x": "LIST:INT",
+                    "y": "INT",
+                    "train_mask": "BOOL",
+                    "name": "STRING",
+                    "is_seed": "BOOL",
+                },
+                "Company": {"x": "FLOAT", "is_seed": "BOOL"},
             },
-            {"Work": ["x", "time"]}, 
-            {"Work": ["y"]}, 
-            {"Work": ["is_train"]}, 
-            {"Work": {"FromVertexTypeName": "People", "ToVertexTypeName": "People",
-                "x": "DOUBLE", "time": "INT", "y": "INT", "is_train": "BOOL"}},
+            {"Colleague": ["x", "time"]},
+            {"Colleague": ["y"]},
+            {"Colleague": ["is_train"]},
+            {
+                "Colleague": {
+                    "FromVertexTypeName": "People",
+                    "ToVertexTypeName": "People",
+                    "IsDirected": False,
+                    "x": "DOUBLE",
+                    "time": "INT",
+                    "y": "INT",
+                    "is_train": "BOOL",
+                },
+                "Work": {
+                    "FromVertexTypeName": "People",
+                    "ToVertexTypeName": "Company",
+                    "IsDirected": False,
+                }
+            },
             False,
             True,
-            True
+            True,
         )
         data = data_q.get()
         self.assertIsInstance(data, pygHeteroData)
-        assert_close_torch(data["Work"]["edge_index"], torch.tensor([[0, 1], [1, 0]]))
-        assert_close_torch(data["Work"]["edge_feat"], 
-            torch.tensor([[0.1, 2021], [1.5, 2020]], dtype=torch.double))
-        assert_close_torch(data["Work"]["edge_label"], torch.tensor([1, 0]))
-        assert_close_torch(data["Work"]["is_train"], torch.tensor([False, True]))
-        assert_close_torch(data["People"]["x"], torch.tensor([[1, 0, 0, 1], [1, 0, 0, 1]]))
+        assert_close_torch(
+            data["Colleague"]["edge_index"], torch.tensor([[0, 1], [1, 0]])
+        )
+        assert_close_torch(
+            data["Colleague"]["edge_feat"],
+            torch.tensor([[0.1, 2021], [1.5, 2020]], dtype=torch.double),
+        )
+        assert_close_torch(data["Colleague"]["edge_label"], torch.tensor([1, 0]))
+        assert_close_torch(data["Colleague"]["is_train"], torch.tensor([False, True]))
+        assert_close_torch(
+            data["People"]["x"], torch.tensor([[1, 0, 0, 1], [1, 0, 0, 1]])
+        )
         assert_close_torch(data["People"]["y"], torch.tensor([1, 1]))
         assert_close_torch(data["People"]["train_mask"], torch.tensor([False, True]))
         assert_close_torch(data["People"]["is_seed"], torch.tensor([True, False]))
         self.assertListEqual(data["People"]["name"], ["Alex", "Bill"])
-        assert_close_torch(data["Company"]["x"], torch.tensor([0.3], dtype=torch.double))
+        assert_close_torch(
+            data["Company"]["x"], torch.tensor([0.3], dtype=torch.double)
+        )
         assert_close_torch(data["Company"]["is_seed"], torch.tensor([False]))
+        print(data["Work"])
+        # assert_close_torch(
+        #     data["Work"]["edge_index"], torch.tensor([[0, 1], [0, 0]])
+        # )
         data = data_q.get()
         self.assertIsNone(data)
 
@@ -358,7 +410,7 @@ class TestGDSBaseLoader(unittest.TestCase):
         exit_event = Event()
         raw = (
             "99,1 0 0 1 ,1,0,Alex,1\n8,1 0 0 1 ,1,1,Bill,0\n",
-            "99,8,0.1,2021,1,0\n8,99,1.5,2020,0,1\n"
+            "99,8,0.1,2021,1,0\n8,99,1.5,2020,0,1\n",
         )
         read_task_q.put(raw)
         read_task_q.put(None)
@@ -378,14 +430,18 @@ class TestGDSBaseLoader(unittest.TestCase):
                 "name": "STRING",
                 "is_seed": "BOOL",
             },
-            ["x", "time"], ["y"], ["is_train"], 
-            {"x": "DOUBLE", "time": "INT", "y": "BOOL", "is_train": "BOOL"} 
+            ["x", "time"],
+            ["y"],
+            ["is_train"],
+            {"x": "DOUBLE", "time": "INT", "y": "BOOL", "is_train": "BOOL"},
         )
         data = data_q.get()
         self.assertIsInstance(data, pygData)
         assert_close_torch(data["edge_index"], torch.tensor([[0, 1], [1, 0]]))
-        assert_close_torch(data["edge_feat"], 
-            torch.tensor([[0.1, 2021], [1.5, 2020]], dtype=torch.double))
+        assert_close_torch(
+            data["edge_feat"],
+            torch.tensor([[0.1, 2021], [1.5, 2020]], dtype=torch.double),
+        )
         assert_close_torch(data["edge_label"], torch.tensor([True, False]))
         assert_close_torch(data["is_train"], torch.tensor([False, True]))
         assert_close_torch(data["x"], torch.tensor([[1, 0, 0, 1], [1, 0, 0, 1]]))
