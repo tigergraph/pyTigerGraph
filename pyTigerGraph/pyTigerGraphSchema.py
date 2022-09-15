0c607f4ff090e58b7c0851cbd5bd10bf5ad4b2eb
Szilard Barany <szilard.barany@tigergraph.com>
2022-09-15 23:11:13 +0100

    Logging WIP2

--- pyTigerGraph/pyTigerGraphSchema.py
+++ pyTigerGraph/pyTigerGraphSchema.py
@@ -12,6 +12,7 @@ from pyTigerGraph.pyTigerGraphBase import pyTigerGraphBase
 
 logger = logging.getLogger(__name__)
 
+
 class pyTigerGraphSchema(pyTigerGraphBase):
 
     def _getUDTs(self) -> dict:
