e8415e3867906775e35411b98c48af3769087d27
Bill Shi <bill.shi@tigergraph.com>
2022-09-30 15:22:54 -0700

    feat: refactor and add Datasets class

---
+++ pyTigerGraph/pyTigerGraphDataset.py
@@ -0,0 +1,54 @@
+"""Data Ingestion Functions
+
+Ingest in-stock datasets into a TigerGraph database.
+All functions in this module are called as methods on a link:https://docs.tigergraph.com/pytigergraph/current/core-functions/base[`TigerGraphConnection` object]. 
+"""
+from .datasets import Datasets
+
+
+class pyTigerGraphDataset:
+    def ingest(self, dataset: Datasets, cleanup=True) -> None:
+        if not dataset.ingest_ready:
+            raise Exception("This dataset is not ingestable.")
+
+        print("---- Checking database ----", flush=True)
+        if self.check_exist_graphs(dataset.name):
+            # self.gsql("USE GRAPH {}\nDROP JOB ALL\nDROP GRAPH {}".format(
+            #     dataset.name, dataset.name
+            # ))
+            print(
+                "A graph with name {} already exists in the database. "
+                "Please drop it first before ingesting.".format(dataset.name)
+            )
+            return
+
+        print("---- Creating graph ----", flush=True)
+        resp = dataset.create_graph(self)
+        print(resp, flush=True)
+        if "Failed" in resp:
+            return
+
+        print("---- Creating schema ----", flush=True)
+        resp = dataset.create_schema(self)
+        print(resp, flush=True)
+        if "Failed" in resp:
+            return
+
+        print("---- Creating loading job ----", flush=True)
+        resp = dataset.create_load_job(self)
+        print(resp, flush=True)
+        if "Failed" in resp:
+            return
+
+        print("---- Ingesting data ----", flush=True)
+        self.graphname = dataset.name
+        resp = dataset.run_load_job(self)
+        print(resp, flush=True)
+
+        if cleanup:
+            print("---- Cleaning ----", flush=True)
+            dataset.clean_up()
+
+    def check_exist_graphs(self, name: str) -> bool:
+        resp = self.gsql("ls")
+        return "Graph {}".format(name) in resp
