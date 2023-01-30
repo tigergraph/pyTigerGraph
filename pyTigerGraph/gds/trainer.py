082a86706927fbeac4994121a81e85b454c1730b
Parker Erickson <parker.erickson@tigergraph.com>
2023-01-30 16:33:53 -0600

    feat(default callback): define default callback

--- pyTigerGraph/gds/trainer.py
+++ pyTigerGraph/gds/trainer.py
@@ -1,46 +1,76 @@
 from .dataloaders import BaseLoader
-from typing import Union, List
+from typing import Union, List, Callable
 
-class TrainerState():
+
+class BaseCallback():
     def __init__(self):
-        self.eval_loss = None
-        self.eval_logits = None
-        self.eval_labels = None
+        pass
+
+    def on_init_end(self):
+        pass
+
+    def on_train_step_end(self):
+        pass
+
+    def on_epoch_end(self):
+        pass
+
+    def on_eval_step_end(self):
+        pass
+
+    def on_eval_end(self):
+        pass
+
+
+class PrinterCallback(BaseCallback):
+    def __init__(self):
+        pass
+
+    def on_train_step_end(self, trainer):
+        print(trainer.train_step_metrics)
+
+    def on_eval_end(self, trainer):
+        print(trainer.eval_global_metrics)
         
-        self.train_loss = None
-        self.train_logits = None
-        self.train_labels = None
 
-    def update_train_state(self, loss, logits, labels):
-        self.train_loss = loss
-        self.train_logits = logits
-        self.train_labels = labels
+class DefaultCallback(BaseCallback):
+    def __init__(self, output_dir="./logs"):
+        self.output_dir = output_dir
+        self.best_loss = float("inf")
 
-    def update_eval_state(self, loss, logits, labels):
-        self.eval_loss = loss
-        self.eval_logits = logits
-        self.eval_labels = labels
+    def on_train_step_end(self, trainer):
+        pass
+    def on_eval_end(self, trainer):
+        pass
+
 
 class Trainer():
     def __init__(self, 
                  model,
                  training_dataloader: BaseLoader,
                  eval_dataloader: BaseLoader,
-                 metrics = None, 
+                 callbacks: List[BaseCallback] = [PrinterCallback],
+                 metrics = None,
                  learning_rate = 0.001, 
                  weight_decay = 0,
                  target_type = None,
-                 loss_fn=None, 
-                 optimizer=None):
+                 loss_fn = None, 
+                 optimizer = None):
         try:
             import torch
         except:
             raise Exception("PyTorch is required to use the trainer. Please install PyTorch.")
-        self.state = TrainerState()
         self.model = model
         self.train_loader = training_dataloader
         self.eval_loader = eval_dataloader
         self.loss_fn = loss_fn
+        self.callbacks = callbacks
+        if metrics:
+            self.metrics = metrics
+        elif self.model.metrics:
+            self.metrics = self.model.metrics
+        else:
+            print("No metrics class defined, only calculating loss")
         if optimizer:
             self.optimizer = optimizer
         else:
@@ -56,15 +86,24 @@ class Trainer():
         else:
             self.target_type = target_type
 
+        for callback in self.callbacks: # instantiate callbacks if not already done so
+            if isinstance(callback, type):
+                callback = callback()
 
-    def train(self, num_epochs=None, num_steps=None, valid_freq=None):
+        for callbacks in self.callbacks:
+            callbacks.on_init_end(trainer=self)
+
+
+    def train(self, num_epochs=None, max_num_steps=None, valid_freq=None):
         if num_epochs:
-            num_steps = self.train_loader.num_batches * num_epochs
+            max_num_steps = self.train_loader.num_batches * num_epochs
         if not(valid_freq):
             valid_freq = self.train_loader.num_batches
         cur_step = 0
-        while cur_step < num_steps:
+        while cur_step < max_num_steps:
             for batch in self.train_loader:
+                if cur_step >= max_num_steps:
+                    break
                 out = self.model(batch)
                 loss = self.model.compute_loss(out,
                                                batch,
@@ -73,14 +112,41 @@ class Trainer():
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
+                if self.metrics:
+                    self.metrics.update_metrics(loss, out, batch)
+                    self.train_step_metrics = self.metrics.get_metrics()
+                    self.train_step_metrics["global_step"] = cur_step
+                    self.train_step_metrics["epoch"] = cur_step/self.train_loader.num_batches
+                    self.metrics.reset_metrics()
+                else:
+                    self.train_step_metrics = {}
                 cur_step += 1
-            if cur_step % valid_freq == 0:
-                self.model.eval()
-                for batch in self.eval_loader:
-                    out = self.model(batch)
-                    loss = self.model.compute_loss(out,
-                                                batch,
-                                                self.target_type,
-                                                loss_fn = self.loss_fn)
-                    self.state.update_eval_state(loss, out, )
-                self.model.train()
+                for callback in self.callbacks:
+                    callback.on_train_step_end(self)
+                if self.eval_loader:
+                    if cur_step % valid_freq == 0:
+                        self.eval()
+
+            for callback in self.callbacks:
+                callback.on_epoch_end(self)             
+
+    def eval(self):
+        self.model.eval()
+        for batch in self.eval_loader:
+            out = self.model(batch)
+            loss = self.model.compute_loss(out,
+                                        batch,
+                                        self.target_type,
+                                        loss_fn = self.loss_fn)
+            if self.metrics:
+                self.metrics.update_metrics(loss, out, batch)
+            for callback in self.callbacks:
+                callback.on_eval_step_end(self)
+        if self.metrics:
+            self.eval_global_metrics = self.metrics.get_metrics()
+        else:
+            self.eval_global_metrics = {}
+        for callback in self.callbacks:
+            callback.on_eval_end(self)
+        self.metrics.reset_metrics()
+        self.model.train()
