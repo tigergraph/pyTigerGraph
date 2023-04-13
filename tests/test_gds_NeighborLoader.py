67aaac90b8a9a95c1cc17c3681b6d1009680aaa2
Bill Shi <bill.shi@tigergraph.com>
2023-04-12 21:50:37 -0700

    fix(tests): fix offset for distributed loader

--- tests/test_gds_NeighborLoader.py
+++ tests/test_gds_NeighborLoader.py
@@ -212,32 +212,32 @@ class TestGDSNeighborLoaderKafka(unittest.TestCase):
 
     @staticmethod
     def run_loader(params: dict) -> int:
-            neighbor_loader = NeighborLoader(
-                graph=params["conn"],
-                batch_size=8,
-                num_neighbors = 10,
-                num_hops =2,
-                v_in_feats = ["x"],
-                v_out_labels = ["y"],
-                v_extra_feats = ["train_mask", "val_mask", "test_mask"],
-                output_format = "PyG",
-                shuffle=False,
-                filter_by = "train_mask",
-                timeout=300000,
-                kafka_address="kafka:9092",
-                kafka_num_partitions=2,
-                kafka_auto_offset_reset="latest",
-                kafka_group_id="test_group",
-                kafka_auto_del_topic = False,
-                kafka_skip_produce = params["kafka_skip_produce"]
-            )
-            num_batches = 0
-            num_seeds = 0
-            for data in neighbor_loader:
-                # print(num_batches, data, data["is_seed"].sum().item(), flush=True)
-                num_batches += 1
-                num_seeds += data["is_seed"].sum().item()
-            return num_batches, num_seeds
+        neighbor_loader = NeighborLoader(
+            graph=params["conn"],
+            batch_size=8,
+            num_neighbors = 10,
+            num_hops =2,
+            v_in_feats = ["x"],
+            v_out_labels = ["y"],
+            v_extra_feats = ["train_mask", "val_mask", "test_mask"],
+            output_format = "PyG",
+            shuffle=False,
+            filter_by = "train_mask",
+            timeout=300000,
+            kafka_address="kafka:9092",
+            kafka_num_partitions=2,
+            kafka_auto_offset_reset="earliest",
+            kafka_group_id="test_group",
+            kafka_auto_del_topic = False,
+            kafka_skip_produce = params["kafka_skip_produce"]
+        )
+        num_batches = 0
+        num_seeds = 0
+        for data in neighbor_loader:
+            # print(num_batches, data, data["is_seed"].sum().item(), flush=True)
+            num_batches += 1
+            num_seeds += data["is_seed"].sum().item()
+        return num_batches, num_seeds
     
     def test_distributed_loaders(self):
         params = [
