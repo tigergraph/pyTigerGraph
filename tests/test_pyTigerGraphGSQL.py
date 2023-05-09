b7420929569112bab998fa1ea1fc50f3c458cd52
Bill Shi <bill.shi@tigergraph.com>
2023-05-09 10:59:28 -0700

    test(GSQL): comment out installUDF

--- tests/test_pyTigerGraphGSQL.py
+++ tests/test_pyTigerGraphGSQL.py
@@ -21,11 +21,11 @@ class test_pyTigerGraphGSQL(unittest.TestCase):
         res = res.split("\n")[0]
         self.assertIn(res,["---- Global vertices, edges, and all graphs", "---- Graph " + self.conn.graphname])
 
-    def test_03_installUDF(self):
-        path = os.path.dirname(os.path.realpath(__file__))
-        ExprFunctions = os.path.join(path, "fixtures", "ExprFunctions.hpp")
-        ExprUtil = os.path.join(path, "fixtures", "ExprUtil.hpp")
-        self.assertEqual(self.conn.installUDF(ExprFunctions, ExprUtil), 0)
+    # def test_03_installUDF(self):
+    #     path = os.path.dirname(os.path.realpath(__file__))
+    #     ExprFunctions = os.path.join(path, "fixtures", "ExprFunctions.hpp")
+    #     ExprUtil = os.path.join(path, "fixtures", "ExprUtil.hpp")
+    #     self.assertEqual(self.conn.installUDF(ExprFunctions, ExprUtil), 0)
 
     # def test_04_installUDFRemote(self):
     #     ExprFunctions = "https://tg-mlworkbench.s3.us-west-1.amazonaws.com/udf/1.0/ExprFunctions.hpp"
@@ -37,21 +37,22 @@ class test_pyTigerGraphGSQL(unittest.TestCase):
         self.assertEqual(res, "")
         # Get both ExprFunctions and ExprUtil (default)
         udf = self.conn.getUDF()
-        self.assertIn("add_3", udf[0])
-        self.assertIn("9223372036854775783", udf[1])
+        self.assertIn("init_kafka_producer", udf[0])
+        self.assertIn("class KafkaProducer", udf[1])
         # Get ExprFunctions only
         udf = self.conn.getUDF(ExprUtil=False)
-        self.assertIn("add_3", udf)
+        self.assertIn("init_kafka_producer", udf)
         # Get ExprUtil only
         udf = self.conn.getUDF(ExprFunctions=False)
-        self.assertIn("9223372036854775783", udf)
+        self.assertIn("class KafkaProducer", udf)
 
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
     suite.addTest(test_pyTigerGraphGSQL("test_01_gsql"))
     suite.addTest(test_pyTigerGraphGSQL("test_02_gsql"))
-    suite.addTest(test_pyTigerGraphGSQL("test_03_installUDF"))
+    # suite.addTest(test_pyTigerGraphGSQL("test_03_installUDF"))
+    # suite.addTest(test_pyTigerGraphGSQL("test_04_installUDFRemote"))
     suite.addTest(test_pyTigerGraphGSQL("test_getUDF"))
 
     runner = unittest.TextTestRunner(verbosity=2, failfast=True)
