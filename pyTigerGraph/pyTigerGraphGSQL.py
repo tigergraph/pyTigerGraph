1c2ab805c44c60b1a25d86d8ab9827c3a9ddbafa
Hunaid Shakoor <hunaidshakoor@TigerGraph-XYVL2DG72P.local>
2024-05-16 14:59:37 -0700

    Added check for substring in newer version loading jobs

--- pyTigerGraph/pyTigerGraphGSQL.py
+++ pyTigerGraph/pyTigerGraphGSQL.py
@@ -58,7 +58,7 @@ class pyTigerGraphGSQL(pyTigerGraphBase):
                 if "Successfully created loading jobs" not in resp:
                     raise TigerGraphException(resp)
             if "RUN LOADING JOB" in query.upper():
-                if "LOAD SUCCESSFUL to TigerGraph" or "LOAD SUCCESSFUL for loading jobid" not in resp: not in resp:
+                if "LOAD SUCCESSFUL to TigerGraph" or "LOAD SUCCESSFUL for loading jobid" not in resp:
                     raise TigerGraphException(resp)
                 
         def clean_res(resp: list) -> str:
