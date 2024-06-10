34f05cb4341d760232365659f8a041a266cd79db
Lu Zhou <luzhou@TigerGraph-WYGPJ4M29R.local>
2024-06-10 12:29:45 -0700

    GML-1660 create new graph and generate token

--- tests/test_jwtAuth.py
+++ tests/test_jwtAuth.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+from os.path import exists
 from requests.auth import HTTPBasicAuth
 
 import unittest
@@ -12,10 +13,55 @@ from tests.pyTigerGraphUnitTest import make_connection
 class TestJWTTokenAuth(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.conn = make_connection()
+        server_config = {
+            "host": "http://127.0.0.1",
+            "graphname": "jwttoken",
+            "username": "tigergraph",
+            "password": "tigergraph",
+            "gsqlSecret": "",
+            "restppPort": "9000",
+            "gsPort": "14240",
+            "gsqlVersion": "",
+            "userCert": None,
+            "certPath": None,
+            "sslPort": "443",
+            "tgCloud": False,
+            "gcp": False,
+            "jwtToken": ""
+        }
+
+        path = os.path.dirname(os.path.realpath(__file__))
+        fname = os.path.join(path, "testserver.json")
+
+        if exists(fname):
+            with open(fname, "r") as config_file:
+                config = json.load(config_file)
+            server_config.update(config)
+
+        cls.conn = TigerGraphConnection(
+            host=server_config["host"],
+            graphname=server_config["graphname"],
+            username=server_config["username"],
+            password=server_config["password"],
+            tgCloud=server_config["tgCloud"],
+            restppPort=server_config["restppPort"],
+            gsPort=server_config["gsPort"],
+            gsqlVersion=server_config["gsqlVersion"],
+            useCert=server_config["userCert"],
+            certPath=server_config["certPath"],
+            sslPort=server_config["sslPort"],
+            gcp=server_config["gcp"],
+            jwtToken=server_config["jwtToken"]
+        )
+    # @classmethod
+    # def setUpClass(cls):
+        # cls.conn = make_connection()
 
 
     def test_jwtauth(self):
+        # self.conn.gsql("create graph jwttoken()")
+        self.conn.getToken(self.conn.createSecret())
+
         authheader = self.conn.authHeader
         print (f"authheader from init conn: {authheader}")
         dbversion = self.conn.getVer()
@@ -29,7 +75,7 @@ class TestJWTTokenAuth(unittest.TestCase):
         else:
             pass
         
-        
+
     def _requestJWTToken(self):
         # Define the URL
         url = f"{self.conn.host}:{self.conn.gsPort}/gsqlserver/requestjwttoken"
