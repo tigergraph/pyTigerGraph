85d7bbbaf10a1a660b28eea5f0ebee56450d00e4
Parker Erickson <parker.erickson@tigergraph.com>
2024-11-18 17:28:05 -0600

    bump version to 1.8.1

--- pyTigerGraph/__init__.py
+++ pyTigerGraph/__init__.py
@@ -2,6 +2,6 @@ from pyTigerGraph.pyTigerGraph import TigerGraphConnection
 from pyTigerGraph.pytgasync.pyTigerGraph import AsyncTigerGraphConnection
 from pyTigerGraph.common.exception import TigerGraphException
 
-__version__ = "1.8"
+__version__ = "1.8.1"
 
 __license__ = "Apache 2"
