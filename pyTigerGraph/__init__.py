101228c75100c27f48aaf07323f4e405062c98fc
Chengbiao Jin <chengbiao.jin@tigergraph.com>
2025-06-06 12:56:27 -0700

    release 1.9.0 deprecate copilot apis

--- pyTigerGraph/__init__.py
+++ pyTigerGraph/__init__.py
@@ -2,6 +2,6 @@ from pyTigerGraph.pyTigerGraph import TigerGraphConnection
 from pyTigerGraph.pytgasync.pyTigerGraph import AsyncTigerGraphConnection
 from pyTigerGraph.common.exception import TigerGraphException
 
-__version__ = "1.8.8"
+__version__ = "1.9.0"
 
 __license__ = "Apache 2"
